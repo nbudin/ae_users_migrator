884e11d6ca59b01d709d89be9efd478254ee7a44
Nat Budin <natbudin@gmail.com>
2010-06-29 12:05:16 -0400

    include associations the Rails 3 way

--- export_ae_users.rb
+++ export_ae_users.rb
@@ -28,13 +28,20 @@ class Person < ActiveRecord::Base
   has_one :account
   has_many :open_id_identities
   has_and_belongs_to_many :roles
+
+  def as_json(options = {})
+    json = super(options)
+
+    %w{email_addresses account open_id_identities roles}.each do |assn|
+      json[assn] = self.send(assn).as_json
+    end
+
+    json
+  end
 end
 
 class Role < ActiveRecord::Base
   has_and_belongs_to_many :people
 end
 
-associations = [:email_addresses, :account, :open_id_identities, :roles]
-people = Person.includes(associations).all
-json = people.as_json(:include => associations).to_json
-puts json
+puts Person.includes(:email_addresses, :account, :open_id_identities, :roles).all.to_json
